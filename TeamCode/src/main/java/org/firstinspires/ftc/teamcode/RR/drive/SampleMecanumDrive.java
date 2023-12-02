556ad51a078008b33601ce6145e5822374d89d5c
pigedy100 <ruhosk@gmail.com>
2023-12-02 16:47:06 -0600

    Tune Constants

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RR/drive/SampleMecanumDrive.java
@@ -1,5 +1,17 @@
 package org.firstinspires.ftc.teamcode.RR.drive;
 
+import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.MAX_ACCEL;
+import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.MAX_ANG_ACCEL;
+import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.MAX_ANG_VEL;
+import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.MAX_VEL;
+import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.MOTOR_VELO_PID;
+import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.RUN_USING_ENCODER;
+import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.TRACK_WIDTH;
+import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.encoderTicksToInches;
+import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.kV;
+import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.kA;
+import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.kStatic;
+
 import androidx.annotation.NonNull;
 
 import com.acmerobotics.dashboard.config.Config;
@@ -21,6 +33,7 @@ import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
@@ -36,18 +49,6 @@ import org.firstinspires.ftc.teamcode.RR.util.LynxModuleUtil;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.MAX_ACCEL;
-import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.MAX_ANG_ACCEL;
-import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.MAX_ANG_VEL;
-import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.MAX_VEL;
-import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.MOTOR_VELO_PID;
-import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.RUN_USING_ENCODER;
-import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.TRACK_WIDTH;
-import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.encoderTicksToInches;
-import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.kA;
-import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.kStatic;
-import static org.firstinspires.ftc.teamcode.RR.drive.DriveConstants.kV;
 
 /*
  * Simple mecanum drive hardware implementation for REV hardware.
@@ -123,6 +124,10 @@ public class SampleMecanumDrive extends MecanumDrive {
         }
 
         // TODO: reverse any motors using DcMotor.setDirection()
+        leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
+        rightFront.setDirection(DcMotorSimple.Direction.FORWARD);
+        rightRear.setDirection(DcMotorSimple.Direction.FORWARD);
+        leftRear.setDirection(DcMotorSimple.Direction.REVERSE);
 
         List<Integer> lastTrackingEncPositions = new ArrayList<>();
         List<Integer> lastTrackingEncVels = new ArrayList<>();
